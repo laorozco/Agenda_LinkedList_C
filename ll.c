637773683ee4066975d223a739ad4b5d0220acaa
asilva111 <45497798+asilva111@users.noreply.github.com>
2019-05-09 21:38:28 -0600

    Update ll.c

--- ll.c
+++ ll.c
@@ -17,20 +17,5 @@ int main()
 	
 	setCursor(&results, getHead(&results));
 	
-	Print List
-	printf("<RESULTS LIST>\n\n");
-	printf("\tNumber of entries: %i\n\n", getEntries(&results));
-	
-	int e = 0;
-	do
-	{
-		printf("\t------------------\n");
-		printf("\t\tResult %i:\n", e++);
-		printf("\t\tGrade: %c\n", getGrade(getCursor(&results)));
-		printf("\t\tScore: %i\n", getScore(getCursor(&results)));
-		printf("\t------------------\n");
-		
-		//forward(&results);
-	}
-	while(e < getEntries(&results));
+
 }
